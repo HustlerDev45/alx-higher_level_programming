c5b46f04abe1d715b38065abfd175e30661e159c
HustlerDev45 <abdoul4845@gmail.com>
2023-12-04 12:12:14 -0800

    task5-fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    return "".join(char for char in my_string if char not in 'cC')
+    n_string = my.string.translate({ord(n): None for n in 'cC'})
+    return n_string
