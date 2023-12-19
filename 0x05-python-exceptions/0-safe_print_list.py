ad4360e52cad5841311bb80ef8c048223d7d4f7d
HustlerDev45 <abdoul4845@gmail.com>
2023-12-18 18:13:29 -0800

    fixedT0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@
         int: Real number of elements printed.
 """
 
+
 def safe_print_list(my_list=[], x=0):
     n = 0
     for i in range(x):
@@ -15,5 +16,5 @@ def safe_print_list(my_list=[], x=0):
             n += 1
         except IndexError:
             break
-        print()
-        return (n)
+    print()
+    return (n)
