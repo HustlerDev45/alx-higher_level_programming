3a4f6c18ad76e5396e1c33bad5115279c7c5ddea
HustlerDev45 <abdoul4845@gmail.com>
2023-12-19 13:05:48 -0800

    task4-fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -24,4 +24,4 @@ def list_division(my_list_1, my_list_2, list_length):
             div_result = 0
         finally:
             new_list.append(div_result)
-        return (new_list)
+    return (new_list)
