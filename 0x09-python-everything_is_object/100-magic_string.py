caba14ffc0c0dd24c3264750a1aab2450993a2d3
HustlerDev45 <abdoul4845@gmail.com>
2024-01-05 17:48:36 -0800

    t100_fixed

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(n):
-    magic_string.n = getattr(magic_string, 'n', -1) + 1
-    return ",".join(["BestSchool"] * (getattr(magic_string, 'n', 0) + 1))
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ", ".join(["BestSchool"] * (getattr(magic_string, 'n', 0) + 1))
