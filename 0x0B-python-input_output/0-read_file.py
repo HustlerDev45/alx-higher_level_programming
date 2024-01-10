69685c5a4564aebec034c644b91555986c5653e9
HustlerDev45 <abdoul4845@gmail.com>
2024-01-09 17:15:23 -0800

    task0fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,5 +4,6 @@
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+    with open(filename, 'r', encoding='utf-8') as f:
+        for line in f:
+            print(line, end='')
